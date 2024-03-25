f5b27de5e24b62b50783dd4dffe663c0dfe4d8bf
Miquiis <miquiisoficial@gmail.com>
2024-03-25 15:59:01 -0400

    Added tsconfig and change folder structure

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as core from '@actions/core';
-import * as fs from 'fs';
-import * as crypto from 'crypto';
+import fs from 'fs';
+import crypto from 'crypto';
 
 async function run(): Promise<void> {
     try {
